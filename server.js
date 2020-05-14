c180271150d1d91aa495833faac0193e180eaada
xueyingtan <xueyingt96@gmail.com>
2020-05-14 23:20:04 +1000

    deprecated code removed

--- server.js
+++ server.js
@@ -16,9 +16,7 @@ app.use(express.static('public'))
 
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL,{
-    useNewUrlParser:true
-})
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error',error => console.error(error))
 db.once('open',()=>console.log('Connected to Mongoose'))
