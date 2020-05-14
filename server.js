ac310aa9e85c5aa96994b953a9919aa6db9ad5fd
xueyingtan <xueyingt96@gmail.com>
2020-05-14 23:33:22 +1000

    debugging

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static('public'))
 
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error',error => console.error(error))
 db.once('open',()=>console.log('Connected to Mongoose'))
