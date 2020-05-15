87b7afbb05ecdfc05b9a5faa20f33994a28ebe81
xueyingtan <xueyingt96@gmail.com>
2020-05-15 17:42:05 +1000

    deploying to heroku error attempt 3

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use(express.static('public'))
 
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error',error => console.error(error))
-db.once('open',()=>console.log('Connected to Mongoose'))
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+// const db = mongoose.connection
+// db.on('error',error => console.error(error))
+// db.once('open',()=>console.log('Connected to Mongoose'))
 
 app.use('/',indexRouter)
 
