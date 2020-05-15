aaa1ef296b039aec072e7cafec1e3c184a891b48
xueyingtan <xueyingt96@gmail.com>
2020-05-15 17:31:55 +1000

    catching error attempt2

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/',(req,res)=>{
-    res.render('index').catch((err)=>console.error(err))
+    res.render('index')
+    res.status(statusCode >= 100 && statusCode < 600 ? err.code : 500);
 })
 
 module.exports = router;
